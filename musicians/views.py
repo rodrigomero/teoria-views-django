1f44517bfc770c82c3295ff162907340f40b75af
Rodrigo Méro <meroo.rodrigo@gmail.com>
2022-10-13 19:28:07 -0300

    o

--- musicians/views.py
+++ musicians/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Musician
+from django.forms.models import model_to_dict
 # Create your views here.
+
+class HelloView(APIView):
+    def get(self, request):
+        return Response('Hello Kenzie')
+
+class MusicianView(APIView):
+    def get(self, request):
+        musicians = Musician.objects.all()
+        # musicians_dict = []
+        # for musician in musicians:
+        #     m = model_to_dict(musician)
+        #     musicians_dict.append(m)
+        return Response(musicians.values())
